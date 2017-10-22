6353aa742b46f52d21b52513613f584720f51114
xXxRosxXx <suttaweelukuan@gmail.com>
2017-10-23 00:11:40 +0700

    Translator of assembly is the other project, so there is 2 projects

--- Translator/main.cpp
+++ Translator/main.cpp
@@ -2,27 +2,18 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-#include "operator.h"
 using namespace std;
 int word[65536];
 int sp=65535;
 int hi,lo;
 int main(int argc, char *argv[])
 {
-	
+		
 	for(int i=0;i<=65535;i++)
 	{
 		word[i]=0;
 		//cout<<i<<endl;
 	}
-//	int binarydigitnum=(floor(log(c)/log(2)));
-/*	for(int i=binarydigitnum;i>=0;i--){
 	
-	b=(c>>i)%2;
-	cout<<b<<endl;
-}*/
-	short int s=-2;
-	cout<<s<<endl;
-	cout<<convert16to32(s)<<endl;
-	return 0;
+return 0;	
 }
---
+++ operator.h
@@ -0,0 +1,10 @@
+int convert16to32(short int num)
+{
+/*convert a 16-bit number into a 32-bit integer*/
+return (int) num;
+
+
+
+
+}
+
