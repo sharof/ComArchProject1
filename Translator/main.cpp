7eb369b488e6a235e1536214b3da3bc85d97303d
xXxRosxXx <suttaweelukuan@gmail.com>
2017-10-23 12:15:16 +0700

    condition added

--- Translator/main.cpp
+++ Translator/main.cpp
@@ -1,41 +1,119 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include <cmath>
 #include <sstream>
 #include <fstream>
+#include "condition.h"
 using namespace std;
 char type;
+string arr[5];
+int pc=0;
+void Rcondcheck(){
+		if(!(atoi(arr[2].c_str())>=0&&atoi(arr[2].c_str())<=7&& atoi(arr[3].c_str())>=0&&atoi(arr[3].c_str())<=7&&atoi(arr[4].c_str())>=0&&atoi(arr[4].c_str())<=7))
+  		{
+  		cout<<"Assemble failed, register number can be only between 0-7 at line "<<pc<<endl; 
+ 		 exit(1);
+   		}
+
+}
+void Jcondcheck(){
+		if(!(atoi(arr[2].c_str())>=0&&atoi(arr[2].c_str())<=7&& atoi(arr[3].c_str())>=0&&atoi(arr[3].c_str())<=7))
+  		{
+  		cout<<"Assemble failed, register number can be only between 0-7 at line "<<pc<<endl; 
+ 		 exit(1);
+   		}
+
+}
+void Icondcheck(){
+		if(!(atoi(arr[2].c_str())>=0&&atoi(arr[2].c_str())<=7&& atoi(arr[3].c_str())>=0&&atoi(arr[3].c_str())<=7))
+  		{
+  		cout<<"Assemble failed, register number can be only between 0-7 at line "<<pc<<endl; 
+ 		 exit(1);
+   		}
+if(!(atoi(arr[4].c_str())>=-32768&&atoi(arr[4].c_str())<=32767))
+{
+	cout<<"Assemble failed, immediate value must be within 16 bits at line "<<pc<<endl;
+	exit(1);
+}
+}
 int main(int argc, char *argv[])
 {
-	int pc=0;
-    ifstream file;
+	int binarycode=0;
+	
+	
+
+    ifstream file,tmpfile;
  	string line,findhalt;	
 	file.open("assembly.txt");	
 
 	
+	
+	
+	
+	
+	
+	
+	
 	while(getline(file,line)){
-int binarycode=0;
-	
-	
-string arr[5];
+binarycode=0;
     int i = 0;
     stringstream ssin(line);
     while (ssin.good() && i < 5){
         ssin >> arr[i];
         i++;
     }
-  if( arr[1]=="add" ){  }
-  else if(arr[1]=="nand" ){binarycode+=1<<22;  }
-   else if( arr[1]=="lw" ){binarycode+=2<<22;}
-   else if(  arr[1]=="sw" ){binarycode+=3<<22;}
-   else if(arr[1]=="beq" ){binarycode+=4<<22;}
-  else if( arr[1]=="jalr" ){binarycode+=5<<22;}
+  if( arr[1]=="add" )
+  { 
+  	Rcondcheck();
+  binarycode+=atoi(arr[2].c_str())<<19;
+  binarycode+=atoi(arr[3].c_str())<<16;
+  binarycode+=atoi(arr[4].c_str()); 
+  }
+  else if(arr[1]=="nand" )
+  {
+  			Rcondcheck();
+  	binarycode+=1<<22;
+    binarycode+=atoi(arr[2].c_str())<<19;
+  	binarycode+=atoi(arr[3].c_str())<<16;
+  	binarycode+=atoi(arr[4].c_str()); 
+  }
+   else if( arr[1]=="lw" ){
+   	Icondcheck();
+   binarycode+=2<<22;
+    binarycode+=atoi(arr[2].c_str())<<19;
+  binarycode+=atoi(arr[3].c_str())<<16;
+  if(atoi(arr[4].c_str())<0){binarycode+=65536 ;}
+  binarycode+=atoi(arr[4].c_str()); 
+   }
+   else if(  arr[1]=="sw" ){
+   Icondcheck();
+   binarycode+=3<<22;
+      binarycode+=atoi(arr[2].c_str())<<19;
+  binarycode+=atoi(arr[3].c_str())<<16;
+  if(atoi(arr[4].c_str())<0){binarycode+=65536 ;}
+  binarycode+=atoi(arr[4].c_str()); 
+  }
+   else if(arr[1]=="beq" ){
+   Icondcheck();
+   binarycode+=4<<22;
+      binarycode+=atoi(arr[2].c_str())<<19;
+  binarycode+=atoi(arr[3].c_str())<<16;
+  if(atoi(arr[4].c_str())<0){binarycode+=65536 ;}
+  binarycode+=atoi(arr[4].c_str()); 
+  }
+  else if( arr[1]=="jalr" ){
+  	Jcondcheck();
+  binarycode+=5<<22;}
   else if( arr[1]=="halt" ){binarycode+=6<<22;}
      else if( arr[1]=="noop"  ){binarycode+=7<<22;}
   
-  else{exit(1);}
+  else{
+  cout<<"Assemble failed, unknown instruction at line "<<pc<<endl;
+  exit(1);}
 cout<<binarycode<<endl;
+pc++;
 }
 
 		file.close();
