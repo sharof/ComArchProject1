8f58ebf7ba8898fda3ffcc14b4c3456645795b93
xXxRosxXx <suttaweelukuan@gmail.com>
2017-10-23 10:28:33 +0700

    starting translator

--- Translator/main.cpp
+++ Translator/main.cpp
@@ -2,18 +2,43 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <sstream>
+#include <fstream>
 using namespace std;
-int word[65536];
-int sp=65535;
-int hi,lo;
+char type;
 int main(int argc, char *argv[])
 {
-		
-	for(int i=0;i<=65535;i++)
-	{
-		word[i]=0;
-		//cout<<i<<endl;
-	}
+	int pc=0;
+    ifstream file;
+ 	string line,findhalt;	
+	file.open("assembly.txt");	
+
 	
+	while(getline(file,line)){
+int binarycode=0;
+	
+	
+string arr[5];
+    int i = 0;
+    stringstream ssin(line);
+    while (ssin.good() && i < 5){
+        ssin >> arr[i];
+        i++;
+    }
+  if( arr[1]=="add" ){  }
+  else if(arr[1]=="nand" ){binarycode+=1<<22;  }
+   else if( arr[1]=="lw" ){binarycode+=2<<22;}
+   else if(  arr[1]=="sw" ){binarycode+=3<<22;}
+   else if(arr[1]=="beq" ){binarycode+=4<<22;}
+  else if( arr[1]=="jalr" ){binarycode+=5<<22;}
+  else if( arr[1]=="halt" ){binarycode+=6<<22;}
+     else if( arr[1]=="noop"  ){binarycode+=7<<22;}
+  
+  else{exit(1);}
+cout<<binarycode<<endl;
+}
+
+		file.close();
+
 return 0;	
 }
